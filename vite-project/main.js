e0904193142799116eeb5a29bcd78a9c47bb9418
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-12-08 09:24:40 -0500

    s
    s

--- vite-project/main.js
+++ vite-project/main.js
@@ -24,12 +24,11 @@ document.querySelector('#app').innerHTML = `
 setupCounter(document.querySelector('#counter'))
  */
 
-const URL = `https://api.trakt.tv`;   
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;   
 
 async function getData(URL){
     try {
       const response = await fetch(URL);
-
       console.log(response);
       const data = await response.json();
       console.log(data);
@@ -41,3 +40,5 @@ async function getData(URL){
 getData(URL);
 
 //https://api.trakt.tv
+//https://api.tvmaze.com 
+//https://api.themoviedb.org/api_key=26906062d4fd4de4f857063554f6f6d3
