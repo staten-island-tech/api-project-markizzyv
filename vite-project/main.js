8c6a00b16b71cbb9cbda5cd808f21dad2824b943
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-01-09 08:05:56 -0500

    nm
    m

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,46 +1,43 @@
 import './style.css';
 import { DOMSelectors } from './dom.js';
 
-
-DOMSelectors.form.addEventListener("submit", function(event){
-  event.preventDefault();
-  var movSearch = DOMSelectors.searchBar.value
-  console.log(movSearch)
-  const search = `https://api.themoviedb.org/api_key=${movSearch}`
-   
-function addMovie (){ 
-  arr.forEach((results)=> document.querySelector(".flex-container").insertAdjacentHTML("beforebegin",
- 
-  `<div class="card">
-   <h1 class ="movietitle">${results.original_title}<h1>
-   <h2 class ="movieimg">${results.poster_path}</h2>
-   <p class ="moviedesc">${results.overview}</p>
-   <h3 class ="moviedate">${results.release_date}</h3>
-  </div>`
-
-));
-};
-
-addMovie(search) ;
-
 const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;   
 
-async function getData(URL){
-    try {
-      const response = await fetch(URL);
-      console.log(response);
-      const data = await response.json();
-      console.log(data);
-  
-    } catch (error) {
-      console.log(error);
-   
+// const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY
+const main = document.getElementById(".main");
+
+
+
+/* async function getMovData(searchURL){
+  try{
+    const response = await fetch(searchURL);
+    if (response.status !=200) {
+      throw new Error(response.statusText);
     }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    addMovie(data);
+    if (data.length === 0){
+      document.querySelector(".Error").textContent = "Error";
+      document.querySelector(".flex-container").innerHTML = "";
+
+    } else {
+      document.querySelector(".Error").textContent = "";
+
+    }
+  } catch (error){
+       document.querySelector(".Error").textContent = "Error";
+  }
 }
+getMovData(searchURL)
+*/
+
+ 
 
 
-getData(URL);
-});
+
+
 /* async function searchData(URL){
 
 };
