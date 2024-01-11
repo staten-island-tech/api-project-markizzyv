d2ee1bc1dc814cdbe10c5ce0d6670f9412686a04
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-01-10 23:49:15 -0500

    s
    d

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,69 +2,225 @@ import './style.css';
 import { DOMSelectors } from './dom.js';
 
 
-const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;  
- 
-
-DOMSelectors.form.addEventListener("submit", function(event){
-  event.preventDefault();
+// const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;  
 
 
 
 
-});
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=26906062d4fd4de4f857063554f6f6d3&page=1`;
 
+//insert display cards first
+function movInsert(arr) {
 
-function insertMov (data){
+document.querySelector(".flex-container").innerHTML = '';
 
-
-
-
-  data.forEach(cardData=> {
-    const {title, poster_path, overview} = cardData;
-    document.querySelector(".flex-container").insertAdjacentHTML(afterbegin,
-  //h1 class diff
+    arr.forEach((card)=> {
+      const {title, poster_path, overview} = card;
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie');
+      movieEl.innerHTML = 
+     
+    ` <div class= "card">
+    <h1 class ="movTitle">${title}</h1>  
+    <img src = "https://image.tmdb.org/t/p/w500${poster_path}" class ="movImg">
+    <p class ="movOverview">${overview}</p>
+    </div>
   `
-  <div class= "card">
-  <h1 class ="movTitle">${title}</h1>  
-  <img src = "https://image.tmdb.org/t/p/w500${poster_path}" class ="movImg">
-  <p class ="movOverview">${overview}</p>
-  </div>
-  `
+    document.querySelector(".flex-container").appendChild(movieEl);
 
 
-    )
-
-
-  }
-  )
+  });
 }
-  insertMov(data.results)
 
-
-
-//logging in
-async function getData(URL){
+async function getData(URL) {
   try {
     const response = await fetch(URL);
+    if (response.status !=200) {
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data)
+    movInsert(data.results); 
+    
+    if (data.results.length === 0) {
+      document.querySelector(".error").textContent = "No Results Found: try a good movie instead buddy";
+      document.querySelector(".flex-container").innerHTML = "";
+      return data
+    }
+  } catch (error) {
+    document.querySelector(".error").textContent = "Error Data Cannot Be Found";
+  }
+}
+
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  document.querySelector(".flex-container").innerHTML = "";
+  const movSearch = DOMSelectors.searchBar.value;
+  const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=26906062d4fd4de4f857063554f6f6d3&query=${movSearch}`;
+  movgetData(searchURL);
+/* 
+  function Insert(data){
+    data.forEach((card)=> {
+      const {title, poster_path, overview} = card;
+      document.querySelector(".flex-container").insertAdjacentHTML("afterbegin", 
+       
+      ` <div class= "card">
+      <h1 class ="movTitle">${title}</h1>  
+      <img src = "https://image.tmdb.org/t/p/w500${poster_path}" class ="movImg">
+      <p class ="movOverview">${overview}</p>
+      </div>
+    `
+      document.querySelector(".flex-container").appendChild(movieEl);
+  
+      )
+    });
+  } */
+  
+}) //1 func only ig
+  
+
+
+async function movgetData(searchURL) {
+  try {
+    const response = await fetch(searchURL);
+
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
     console.log(response);
     const data = await response.json();
     console.log(data);
+    movInsert(data.results); //use diff insert function
 
+    if (data.results.length === 0) {
+      document.querySelector(".error").textContent = "No Results Found: try a good movie instead buddy";
+      document.querySelector(".flex-container").innerHTML = "";
+    } else {
+      document.querySelector(".error").textContent = "";
+    }
   } catch (error) {
-    console.log(error);
- 
+    document.querySelector(".error").textContent = "Error fetching data";
   }
 }
+
+
 getData(URL);
 
 
-DOMSelectors.form.addEventListener("submit", function(event){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  function movInsert (arr){
+  arr.forEach((results)=> {
+    document.querySelector(".flex-container").insertAdjacentHTML("afterbegin", 
+    `
+    <div class="card">
+    <h2 class = "movTitle">${results.title}</h2>
+    </div>
+    `
+    )
+  });
+ }
+
+/* DOMSelectors.form.addEventListener("submit", function(event){
   event.preventDefault();
-
-
 });
+ */
 
 
+//logging in
+/* async function getData(URL){
+  try {
+    const response = await fetch(URL);
+    if (response.status !=200){
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    movInsert(data)
+    if (data.length === 0){
+      document.querySelector(".error").textContent = "Error";
+      document.querySelector(".flex-container").innerHTML = "";
+      return data
+    }
+  } catch (error) {
+   // console.log(error);
+   document.querySelector(".error").textContent = "Error";
+ 
+  }
+}
+getData(URL); */
+ 
+/* 
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.querySelector(".flex-container").innerHTML = "";
+  var movSearch = `${DOMSelectors.searchBar.value}`
+  console.log(movSearch)
+  const searchURL = `'https://api.themoviedb.org/3/search/movie?query=${movSearch}&api_key=26906062d4fd4de4f857063554f6f6d3`;
+
+  function Insert(datarr){
+    datarr.forEach((results)=> {
+      document.querySelector(".flex-container").insertAdjacentHTML("afterbegin", 
+      `
+     <div class="card">
+     <h2 class = "movTitle">${results.title}</h2>
+     </div>
+      `
+      )
+    })
+  } */
+/* async function movgetData (searchURL) {
+  try {
+    const response = await fetch(searchURL);
+    if (response.status !=200) {
+      throw new Error(response.statusText);
+
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data)
+    Insert(data)
+    if (data.length === 0) {
+      document.querySelector(".error").textContent = "Error";
+      document.querySelector(".flex-container").innerHTML = "";
+    } else {
+      document.querySelector(".error").textContent = "";
+      return data
+    }
+  
+  } catch (error){
+    document.querySelector(".error").textContent = "Error";
+  }
+}
+
+movgetData(searchURL)
+
+})
+ */
 
   //font for h3, and p 
 
@@ -75,125 +231,3 @@ DOMSelectors.form.addEventListener("submit", function(event){
 
 
 
-
-
-/* 
-
-function Insert(secondarr){
-  secondarr.forEach((results)=> {
-    document.querySelector(".flex-container").insertAdjacentHTML("afterbegin",
-    `<div class="card">
-    <h1 class ="movietitle">${results.original_title}<h1>
-    <h2 class ="movieimg">${results.poster_path}</h2>
-    <p class ="moviedesc">${results.overview}</p>
-    <h3 class ="moviedate">${results.release_date}</h3>
-   </div>`
-    );
-  });
-}
-
-
-async function getData(searchURL){
-    try {
-      const response = await fetch(URL);
-      if (response.status !=200) {
-        throw new Error(response.statusText);
-      }
-      console.log(response);
-      const data = await response.json();
-      console.log(data);
-      Insert(data)
-      if (data.length === 0) {
-        document.querySelector(".Error").textContent = "Error"
-        document.querySelector(".flex-container").textContent = "";
-      }
-    } catch (error) {
- 
-      document.querySelector(".Error").textContent = "Error";
-    }
-}
-
-
-getData(URL);
-
-
-DOMSelectors.form.addEventListener("submit", function(event){
-  event.preventDefault();
-  document.querySelector(".Flex-container").innerHTML = "";
-  var movSearch = `${DOMSelectors.searchBar.value}`
-  console.log(movSearch)
-  const searchURL = `https://www.themoviedb.org/search?query=${movSearch}`
-
-
-function addMovie (arr){
-  arr.forEach((results)=> {
-    document.querySelector(".flex-container").insertAdjacentHTML("afterbegin",
- 
-  `<div class="card">
-   <h1 class ="movietitle">${results.original_title}<h1>
-   <h2 class ="movieimg">${results.poster_path}</h2>
-   <p class ="moviedesc">${results.overview}</p>
-   <h3 class ="moviedate">${results.release_date}</h3>
-  </div>`
-    );
-  });
- };
-
-
-async function getMovData(searchURL){
-  try{
-    const response = await fetch(searchURL);
-    if (response.status !=200) {
-      throw new Error(response.statusText);
-    }
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    addMovie(data);
-    if (data.length === 0){
-      document.querySelector(".Error").textContent = "Error";
-      document.querySelector(".flex-container").innerHTML = "";
-
-
-    } else {
-      document.querySelector(".Error").textContent = "";
-
-
-    }
-  } catch (error){
-       document.querySelector(".Error").textContent = "Error";
-  }
-}
-getMovData(searchURL)
-
-
-});
-
- 
- */
-
-
-
- 
-
-
-
-
-/* async function searchData(URL){
-
-};
-
-searchData(URL)
- */
-
-//const ser = (link)=${shit}
-
-
-
-
-
-
-
-
-
-//https://api.themoviedb.org/api_key=26906062d4fd4de4f857063554f6f6d3
